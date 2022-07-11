c8bc05f5c66313c8d275a1c5685a221bf5f026f8
carlo-ddm <carloduminuco90@gmail.com>
2022-07-11 19:06:27 +0200

    creo seeder e popolo tabella

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = ['Front end', 'Back end', 'Design', 'UX', 'Laravel', 'VueJs'];
+        foreach ($data as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($tag, '-');
+            $new_tag->save();
+        }
+    }
+}
