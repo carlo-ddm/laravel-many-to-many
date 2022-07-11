2bb56aa4a78e1d97f9ccd83f75284fe84fe1685f
carlo-ddm <carloduminuco90@gmail.com>
2022-07-11 19:43:20 +0200

    creo e popolo tab ponte

--- database/seeds/PostsTagsTableSeeder.php
+++ database/seeds/PostsTagsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use App\Tag;
 
 class PostsTagsTableSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class PostsTagsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i=0; $i < 20; $i++) {
+            // estrazione di un post random
+            $post = Post::inRandomOrder()->first();
+            // estraggo random l'id di un tag
+            $tag_id = Tag::inRandomOrder()->first()->id;
+            //inserisco il dato nella tabella ponte (ricorda laravel trova la tab anche se non esiste il model della tab ponte). Con 'attach'viene inserita la relazione nella tabella ponte. Ad attach posso passare un singolo id o un array di id.
+            $post->tags()->attach($tag_id);
+        }
     }
 }
